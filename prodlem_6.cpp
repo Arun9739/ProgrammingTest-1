918e1e77065b077bd378fe254f13aefedd718ba1
Arun G Nayak <nayakarun02@gmail.com>
2022-07-22 21:54:00 +0530

    Added prob 6

---
+++ prodlem_6.cpp
@@ -0,0 +1,33 @@
+
+/* Program to sort names based on alphabetical order */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    string arr[n], temp;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << endl;
+    }
+
+}
